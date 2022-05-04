1627b22d0c9577685b4917e9c08daa4b9da43c50
Daewon Lee <daewon4you@gmail.com>
2022-05-04 14:46:29 +0900

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='lpf',
+    version="0.0.1",
+    description='Ladybird Pattern Formation (LPF)',
+    author='Daewon Lee',
+    author_email='daewon4you@gmail.com',
+    packages=find_packages(),
+
+)
