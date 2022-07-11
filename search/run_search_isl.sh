263025e3d511fdb8c46fcebc4b838be5f4f78be0
Daewon Lee <daewon4you@gmail.com>
2022-07-12 00:54:24 +0900

    Redirect nohup outputs

--- search/run_search_isl.sh
+++ search/run_search_isl.sh
@@ -1,4 +1,4 @@
-nohup python search_isl_haxyridis.py --config config_search_succinea.yaml --gpu 0 &
-nohup python search_isl_haxyridis.py --config config_search_succinea.yaml --gpu 1 &
-nohup python search_isl_haxyridis.py --config config_search_succinea.yaml --gpu 2 &
-nohup python search_isl_haxyridis.py --config config_search_succinea.yaml --gpu 3 &
+nohup python search_isl_haxyridis.py --config config_search_succinea.yaml --gpu 0 > log_search_gpu0.out 2>&1 &
+nohup python search_isl_haxyridis.py --config config_search_succinea.yaml --gpu 1 > log_search_gpu1.out 2>&1 &
+nohup python search_isl_haxyridis.py --config config_search_succinea.yaml --gpu 2 > log_search_gpu2.out 2>&1 &
+nohup python search_isl_haxyridis.py --config config_search_succinea.yaml --gpu 3 > log_search_gpu3.out 2>&1 &
