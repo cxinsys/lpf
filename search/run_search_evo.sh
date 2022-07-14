3acac603761ce6cdc6eb0365f2e4ed00ed70d5d5
Daewon Lee <daewon4you@gmail.com>
2022-07-14 15:31:10 +0900

    Fix run_search_liaw_evo.sh

--- search/run_search_evo.sh
+++
@@ -1,6 +0,0 @@
-nohup python search_evo.py --config ../config/config_search_succinea.yaml --gpu 0 > log_search_evo_gpu0.out 2>&1 &
-sleep 1s
-
-nohup python search_evo.py --config ../config/config_search_succinea.yaml --gpu 1 > log_search_evo_gpu1.out 2>&1 &
-sleep 1s
-
