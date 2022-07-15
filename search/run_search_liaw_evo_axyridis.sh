40fb2ba036bd3fb49ad3b110649cd8d8d215a1d6
Daewon Lee <daewon4you@gmail.com>
2022-07-15 16:02:35 +0900

    Fix running search

--- search/run_search_liaw_evo_axyridis.sh
+++ search/run_search_liaw_evo_axyridis.sh
@@ -1,4 +1,5 @@
-nohup python search_liaw_evo.py --config ../config/config_search_axyridis.yaml --gpu 0 > log_search_evo_gpu0.out 2>&1 &
+nohup python search_liaw_evo.py --config ../config/config_search_axyridis_gpu.yaml --gpu 0 > log_search_evo_gpu0.out 2>&1 &
 sleep 1s
-nohup python search_liaw_evo.py --config ../config/config_search_axyridis.yaml --gpu 1 > log_search_evo_gpu1.out 2>&1 &
+
+nohup python search_liaw_evo.py --config ../config/config_search_axyridis_gpu.yaml --gpu 1 > log_search_evo_gpu1.out 2>&1 &
 sleep 1s
