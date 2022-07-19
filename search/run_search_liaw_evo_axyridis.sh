3add15b40695d7e1b18abfc88d02672f057978f7
Daewon Lee <daewon4you@gmail.com>
2022-07-19 14:32:37 +0900

    Edit config for searching axyridis

--- search/run_search_liaw_evo_axyridis.sh
+++ search/run_search_liaw_evo_axyridis.sh
@@ -1,11 +1,4 @@
 nohup python search_liaw_evo.py --config ../config/config_search_axyridis_gpu.yaml --gpu 0 > log_search_evo_gpu0.out 2>&1 &
-sleep 1s
+sleep 5s
 
 nohup python search_liaw_evo.py --config ../config/config_search_axyridis_gpu.yaml --gpu 1 > log_search_evo_gpu1.out 2>&1 &
-sleep 1s
-
-#nohup python search_liaw_evo.py --config ../config/config_search_axyridis_cpu.yaml > log_search_evo_cpu1.out 2>&1 &
-#sleep 1s
-
-#nohup python search_liaw_evo.py --config ../config/config_search_axyridis_cpu.yaml > log_search_evo_cpu2.out 2>&1 &
-#sleep 1s
