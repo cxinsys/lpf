a429211eb9b6c5783ba2f8e370c14c50fc299eb9
Daewon Lee <daewon4you@gmail.com>
2022-11-15 08:55:55 +0900

    Fix formatting dir names

--- lpf/models/rdmodel.py
+++ lpf/models/rdmodel.py
@@ -59,7 +59,7 @@ class ReactionDiffusionModel(object):
             initializer = self._initializer
 
         batch_size = params.shape[0]
-        dname_individual = "model_%0{}d".format(int(np.floor(np.log10(batch_size))) + 1)
+        dname_model = "model_%0{}d".format(int(np.floor(np.log10(batch_size))) + 1)
 
         if dpath_model:
             fstr_fname_model \
@@ -79,27 +79,27 @@ class ReactionDiffusionModel(object):
 
         if dpath_ladybird:
             for i in range(batch_size):
-                os.makedirs(pjoin(dpath_ladybird, dname_individual%(i+1)), exist_ok=True)
+                os.makedirs(pjoin(dpath_ladybird, dname_model % (i + 1)), exist_ok=True)
             # end of for
 
             fstr_fname_ladybird \
-                = "ladybird_%0{}d.png".format(int(np.floor(np.log10(n_iters))))
+                = "ladybird_%0{}d.png".format(int(np.floor(np.log10(n_iters))) + 1)
 
         if dpath_pattern:
             for i in range(batch_size):
-                os.makedirs(pjoin(dpath_pattern, dname_individual%(i+1)), exist_ok=True)
+                os.makedirs(pjoin(dpath_pattern, dname_model % (i + 1)), exist_ok=True)
             # end of for
 
             fstr_fname_pattern \
-                = "pattern_%0{}d.png".format(int(np.floor(np.log10(n_iters))))
+                = "pattern_%0{}d.png".format(int(np.floor(np.log10(n_iters))) + 1)
             
-        if dpath_states:
-            for i in range(batch_size):
-                os.makedirs(pjoin(dpath_states, dname_individual%(i+1)), exist_ok=True)
-            # end of for
-
-            fstr_fname_states \
-                = "states_%0{}d.png".format(int(np.floor(np.log10(n_iters))))
+        # if dpath_states:
+        #     for i in range(batch_size):
+        #         os.makedirs(pjoin(dpath_states, dname_individual%(i+1)), exist_ok=True)
+        #     # end of for
+        #
+        #     fstr_fname_states \
+        #         = "states_%0{}d.png".format(int(np.floor(np.log10(n_iters))) + 1)
 
         params = self.am.array(params, dtype=params.dtype)
 
@@ -113,20 +113,20 @@ class ReactionDiffusionModel(object):
                 if dpath_ladybird:
                     for j in range(batch_size):
                         fpath_ladybird = pjoin(dpath_ladybird,
-                                               dname_individual%(j+1),
-                                               fstr_fname_ladybird%(i+1))
+                                               dname_model % (j + 1),
+                                               fstr_fname_ladybird % (i+1))
                         if dpath_pattern:
                             fpath_pattern = pjoin(dpath_pattern,
-                                                  dname_individual%(j+1),
-                                                  fstr_fname_pattern%(i+1))
+                                                  dname_model % (j + 1),
+                                                  fstr_fname_pattern % (i+1))
                             self.save_image(j, fpath_ladybird, fpath_pattern)
                         else:
                             self.save_image(j, fpath_ladybird)
                                     
-                if dpath_states:
-                    for j in range(batch_size):
-                        fpath_states = pjoin(dpath_states, dname_individual%(j+1), fstr_fname_states%(i+1))
-                        self.save_states(j, fpath_states)
+                # if dpath_states:
+                #     for j in range(batch_size):
+                #         fpath_states = pjoin(dpath_states, dname_individual%(j+1), fstr_fname_states%(i+1))
+                #         self.save_states(j, fpath_states)
 
                 if verbose >= 1:
                     print("- [Iteration #%d] elapsed time: %.5e sec."%(i+1, time.time() - t_beg))
