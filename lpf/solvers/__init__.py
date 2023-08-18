27437adbd729dd5305287feed1671956cdf19862
Daewon Lee <daewon4you@gmail.com>
2023-08-18 13:39:57 +0900

    Remove scipy solver

--- lpf/solvers/__init__.py
+++ lpf/solvers/__init__.py
@@ -2,6 +2,5 @@ from lpf.solvers.solver import Solver
 from lpf.solvers.eulersolver import EulerSolver
 from lpf.solvers.heunsolver import HeunSolver
 from lpf.solvers.rungekuttasolver import RungeKuttaSolver
-from lpf.solvers.scipysolver import ScipySolver
 
 from lpf.solvers.solverfactory import SolverFactory
